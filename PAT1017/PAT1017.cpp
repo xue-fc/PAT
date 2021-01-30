3e31bee2db4bd4cbd86f7bf9527e7e91800bc52f
xue-fc <xue-fc@126.com>
2021-01-30 09:57:26 +0800

    m

--- PAT1017/PAT1017.cpp
+++ PAT1017/PAT1017.cpp
@@ -9,76 +9,72 @@
 using namespace std;
 
 const int MAX = 10000;
-int toll[24];
-
+int s2i(string a);
 int main(){
-    for(int i=0;i<24;i++){
-        cin>>toll[i];
-    }
-    int N;
-    cin >> N;
+
+    int N,K;
+    cin>>N>>K;
+    getchar();
     char str[MAX];
-    char *token1,*token2,*saveptr1,*saveptr2;
-    map<string,map<string,int>> mymap;
+    char *token;
+    map<int,int> mymap;
     for(int i=0;i<N;i++){
         cin.getline(str,MAX);
-        token1 = strtok_r(str," ",&saveptr1);
-        string id = string(token1);
-        token1 = strtok_r(NULL," ",&saveptr1);
-        string time = string(token1);
-        token1 = strtok_r(NULL," ",&saveptr1);
-        string direction = string(token1);
-        int d = (direction.compare("on-line") ? 0 : 1);
-        map<string,int> tmp;
-        if(mymap.count(id) == 0)
-        {
-            tmp.insert(pair<string,int>(time,d));
-            mymap[id] = tmp;
+        token = strtok(str," ");
+        string tmp = string(token);
+        int t = s2i(tmp);
+        token = strtok(NULL," ");
+        int p_time = atoi(token);
+        mymap[t] = p_time*60;
+    }
+    int t = 8*3600;
+    vector<int> win(K,0);
+    double all = 0;
+    int num=0;
+    for(map<int,int>::iterator it = mymap.begin();it!=mymap.end();it++)
+    {
+        if(it->first >= 17*3600)
+        {    
+            break;
+        }
+        num++;
+        if(K>0){
+            K--;
+            all += ( t > it->first ? t - it->first : 0); 
+            win[K] = (t < it->first ? it->first : t) + it->second;
+            continue;
         }
         else{
-            tmp = mymap[id];
-            tmp.insert(pair<string,int>(time,d));
-        }
-    }
-    for(map<string,map<string,int>>::iterator it=mymap.begin();it!=mymap.end();it++){
-        map<string,int> tmp = it->second;
-        int a = 0;
-        cout<<it->first<<' ';
-        for(map<string,int>::iterator it=tmp.begin();it!=tmp.end();it++){
-            if(it->second == a)
-            {
-                if(a==0)
-                    continue;
+            vector<int>::iterator it_l = min_element(win.begin(),win.end());
+            if(*it_l > it->first)
+            {   
+                all += *it_l - it->first;
+                (*it_l) += it->second;
             }
+            else
+            {
+                *it_l = it->first + it->second;
+            }
+            
         }
     }
-
+    double ave = all/60.0; 
+    printf("%.1f\n",ave/num);
     return 0;
 }
 
-void count_cost(string a,string b){
-    char *token;
-    char str[MAX];
-    int start[3];//日时分
-    int end[3];
-    string month;
-    strcpy(str,a.c_str());
-    token = strtok(str,":");
-    month = string(token);
-    for(int i=0;i<3;i++){
-        token = strtok(NULL,":");
-        start[i] = atoi(token);
-    }
-    strcpy(str,a.c_str());
-    token = strtok(str,":");
-    for(int i=0;i<3;i++){
-        token = strtok(NULL,":");
-        end[i] = atoi(token);
-    }
-
-    int minutes=0,cost=0;
-    int s = (start[0] - 1)*24*60 + start[1]*60 + start[3];
-    int e = (end[0] - 1)*24*60 + end[1]*60 + end[3];
-    minutes = s - e;
+int s2i(string a){
+    const char *str = a.c_str();
+    char t[MAX],*token,*s;
+    int time[3];
+    strcpy(t,str);
+    
+    token = strtok_r(t,":",&s);
+    time[0] = atoi(token);
+    token = strtok_r(NULL,":",&s);
+    time[1] = atoi(token);
+    token = strtok_r(NULL,":",&s);
+    time[2] = atoi(token);
+    
+    return (60*time[0] + time[1])*60 + time[2];
 }
-
